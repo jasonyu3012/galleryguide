f042be9a4e1c194c23390908dbd8c75a5740c774
Tara Sophia Roshan <troshan@utexas.edu>
2023-02-15 08:47:05 -0600

    add model cards on Home, model pages
    Still need to add instance cards and fill out their information.
    Will add the cards, let Jerry know current progress, and then go
    to class!

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <p>Motivation for GalleryGuide, paste from readme.</p>
+      <CardGroup>
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Artworks</Card.Title>
+            <Card.Text>
+              View our artwork instances.
+            </Card.Text>
+            <Link to='/artworks'>
+              <Button>Artworks</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Artists</Card.Title>
+            <Card.Text>
+              View our artist instances.
+            </Card.Text>
+            <Link to='/artists'>
+              <Button>Artists</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Galleries</Card.Title>
+            <Card.Text>
+              View our gallery instances.
+            </Card.Text>
+            <Link to='/galleries'>
+              <Button>Galleries</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
