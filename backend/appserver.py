d85f54b9361e8625b8c83e817e69e0c460ebc90b
Landon Johnson <landon@cs.utexas.edu>
2023-02-15 09:10:14 -0600

    Added some server code that should be able to serve the frontend correctly. When `npm run build` is run, we need to point `static_folder` and `static_url_path` to the build folder.

--- backend/appserver.py
+++ backend/appserver.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+
+app = Flask(__name__, static_folder="app", static_url_path="/app")
+
+"""
+Redirects urls with no routes back to the react-router in index.html
+"""
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
