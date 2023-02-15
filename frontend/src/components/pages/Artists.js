33d980238cb808563196cb156e3c2ee155e02b6d
unknown <jerryming2002@gmail.com>
2023-02-15 17:39:57 -0600

    test commit with updates model pages

--- frontend/src/components/pages/Artists.js
+++ frontend/src/components/pages/Artists.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const Artists = () => {
   return (
     <div>
       <h1>Artists</h1>
+      <CardGroup>
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Grant Wood</Card.Title>
+            <Card.Text>
+              Grant Wood is known for his stylized and subtly humorous scenes of rural people, Iowa cornfields, and mythic subjects from American history
+            </Card.Text>
+            <Link to='/GrantWood'>
+              <Button>Read More</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Vincent van Gogh</Card.Title>
+            <Card.Text>
+              A Dutch Post-Impressionist painter who posthumously became one of the most famous and influential figures in Western art history. 
+            </Card.Text>
+            <Link to='/VincentvanGogh'>
+              <Button>Read More</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Mary Cassat</Card.Title>
+            <Card.Text>
+              The National Gallery of Art is a museum in Washington, D.C. that hosts a variety of artworks, collected primarily though donation.  
+            </Card.Text>
+            <Link to='/NationalGallery'>
+              <Button>Explore the National Gallery of Art</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
--- frontend/src/components/pages/Galleries.js
+++ frontend/src/components/pages/Galleries.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const Galleries = () => {
   return (
     <div>
       <h1>Galleries</h1>
+      <CardGroup>
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>Art Institute of Chicago</Card.Title>
+            <Card.Text>
+              The Art Institute of Chicago is the second largest art museum in the USA housing a world-renowned encyclopedic collection.
+            </Card.Text>
+            <Link to='/ArtInstituteChicago'>
+              <Button>Explore the Art Institute of Chicago</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>MET Museum</Card.Title>
+            <Card.Text>
+              The Metropolitan Museum of Art, also known as the MET, is the largest and most visited Museum in the United States. 
+            </Card.Text>
+            <Link to='/TheMet'>
+              <Button>Explore the MET Museum</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '15rem', justifyContent: 'center' }}>
+          {/* add in image! */}
+          <Card.Img variant="top" src=''/>
+          <Card.Body>
+            <Card.Title>National Gallery of Art</Card.Title>
+            <Card.Text>
+              The National Gallery of Art is a museum in Washington, D.C. that hosts a variety of artworks, collected primarily though donation.  
+            </Card.Text>
+            <Link to='/NationalGallery'>
+              <Button>Explore the National Gallery of Art</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+
     </div>
   );
 }
