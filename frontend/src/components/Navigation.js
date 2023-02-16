df026a27753168f26030fcc342046e62d958f445
Tara Sophia Roshan <troshan@utexas.edu>
2023-02-15 23:36:25 -0600

    testing navbar linking

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,18 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">GalleryGuide</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">GalleryGuide</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/artworks">Artworks</Nav.Link>
-            <Nav.Link href="/artists">Artists</Nav.Link>
-            <Nav.Link href="/galleries">Galleries</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/artworks">Artworks</Nav.Link>
+            <Nav.Link as={Link} to="/artists">Artists</Nav.Link>
+            <Nav.Link as={Link} to="/galleries">Galleries</Nav.Link>
           </Nav>
       </Container>
     </Navbar>
