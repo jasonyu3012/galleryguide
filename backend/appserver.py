614a5773f3391b9a769538fdba223c54f7a1cea0
Jason Yu <jasonyu3012@gmail.com>
2023-02-15 23:51:39 -0600

    added build folder to backend and appserver.py

--- backend/appserver.py
+++ backend/appserver.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask.helpers import send_from_directory
 
-
-app = Flask(__name__, static_folder="mock_build", static_url_path="/mock_build")
+app = Flask(__name__, static_folder='build', static_url_path='/')
 
 """
 Redirects urls with no routes back to the react-router in index.html
 """
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
-def catch_all(path):
-    return app.send_static_file("index.html")
+def serve(path):
+    print("serve")
+    return app.send_static_file('index.html')
+
+# @app.route("/about")
+# def about():
+#     return "<h1>Hello there</h1>"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
