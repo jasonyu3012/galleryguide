c48dac3a5c21eb8f80c0c7b128f0d2b6c69b545a
Tara Sophia Roshan <troshan@utexas.edu>
2023-03-06 22:47:39 -0600

    Update test paths

--- frontend/gui_tests.py
+++ frontend/gui_tests.py
@@ -5,14 +5,14 @@ from sys import platform
 if __name__ == "__main__":
     # Use chromedriver based on OS
     if platform == "win32":
-        PATH = "./frontend/testing/chromedriver.exe"
+        PATH = "./testing/chromedriver.exe"
     elif platform == "linux":
-        PATH = "./frontend/testing/chromedriver_linux"
+        PATH = "./testing/chromedriver_linux"
     else:
         print("Unsupported OS")
         exit(-1)
 
     # Run all of the GUI tests
-    os.system("python3 ./frontend/testing/splashpage_tests.py")
-    os.system("python3 ./frontend/testing/navigation_tests.py")
-    os.system("python3 ./frontend/testing/instance_tests.py")
+    os.system("python3 ./testing/splashpage_tests.py")
+    os.system("python3 ./testing/navigation_tests.py")
+    os.system("python3 ./testing/instance_tests.py")
