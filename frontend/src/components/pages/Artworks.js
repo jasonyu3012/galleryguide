cb82782ddb797782ca4f6468148e7edd0d0b79b0
Tara Sophia Roshan <troshan@utexas.edu>
2023-03-19 21:20:40 -0500

    adds pagination but with too many requests (non-functional)

--- frontend/src/components/pages/Artworks.js
+++ frontend/src/components/pages/Artworks.js
@@ -4,11 +4,13 @@ import React from 'react';
 // Library imports
 import axios from 'axios';
 import { Button, Card, Col, Row } from 'react-bootstrap';
+import Pagination from 'react-bootstrap/Pagination';
 // Local imports
 import './InstanceModels.css';
 
 const ARTWORKS_NUM_PAGES = 888;
 export const ARTWORKS_NUM_IDS = 7986;
+
 
 export default class Artworks extends React.Component {
   constructor(props) {
@@ -16,13 +18,15 @@ export default class Artworks extends React.Component {
     this.state = {
       databaseResponse: [],
       data: [],
-      pageIndex: 1
+      pageIndex: 1,
     };
   }
 
   // TODO #33 implement pagination
-  getResponseData() {
-    axios.get(`https://galleryguide.me/api/artworks?page=${this.state.pageIndex}`)
+  getResponseData = (targetIndex) => {
+    console.log("getting response data")
+    // TODO do some checks
+    axios.get(`https://galleryguide.me/api/artworks?page=${ targetIndex }`)
       .then(response => {
         console.log(this.url)
         const responseData = response.data
@@ -33,25 +37,50 @@ export default class Artworks extends React.Component {
 
         this.setState({ databaseResponse: responseData })
         this.setState({ data: responseData.artworks })
+        this.setState({ pageIndex: targetIndex })
       })
       .catch((error) => {
         console.log("axios error: ", error)
       })
   }
 
+  // Set up pagination items
+  paginationSetup() {
+    return (
+      <Pagination>
+        <Pagination.First onClick={this.getResponseData(1)}/>
+        <Pagination.Prev onClick={this.getResponseData(this.state.pageIndex - 1)}/>
+        <Pagination.Item onClick={this.getResponseData(5)} key={5} active={5 === this.state.pageIndex}>
+          {5}</Pagination.Item>
+        <Pagination.Ellipsis/>
+
+        <Pagination.Item onClick={this.getResponseData(10)} key={10} active={10 === this.state.pageIndex}>
+          {10}</Pagination.Item>
+
+        <Pagination.Ellipsis/>
+        <Pagination.Item onClick={this.getResponseData(ARTWORKS_NUM_PAGES)} key={ARTWORKS_NUM_PAGES} active={ARTWORKS_NUM_PAGES === this.state.pageIndex}>
+          { ARTWORKS_NUM_PAGES }</Pagination.Item>
+        <Pagination.Next onClick={this.getResponseData(this.state.pageIndex + 1)}/>
+        <Pagination.Last onClick={this.getResponseData(ARTWORKS_NUM_PAGES)}/>
+      </Pagination>
+    )
+  }
+
   // Run once the page has loaded
   componentDidMount() {
     console.log("page loaded")
-    this.getResponseData()
+    this.getResponseData(1)
   }
 
   render() {
     return (
       <div>
         <h1>Artworks</h1>
-        <p>Showing page {this.state.pageIndex}/{ARTWORKS_NUM_PAGES}, 9/{ARTWORKS_NUM_IDS} artworks.</p>
+        <p>Showing page { this.state.pageIndex }/{ ARTWORKS_NUM_PAGES }, 9/{ ARTWORKS_NUM_IDS } artworks.</p>
+        { this.paginationSetup() }
+        <h1>test</h1>
         {
-          <Row xs={ 1 } md={ 3 } className="g-4">
+          <Row xs={1} md={3} className="g-4">
             { this.state.data.map(entry => (
               <Col>
                 <Card style={{ justifyContent: 'center' }} key={ entry.id }>
