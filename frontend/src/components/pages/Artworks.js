2651d001a5d15ce4518a5587508f5b5bef088dd2
Jerry Ming <jerryming2002@gmail.com>
2023-03-23 22:22:20 -0500

    cleaning

--- frontend/src/components/pages/Artworks.js
+++ frontend/src/components/pages/Artworks.js
@@ -1,68 +1,52 @@
-// React imports
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import React from 'react';
-import ReactPaginate from 'react-paginate';
-// Library imports
 import axios from 'axios';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
 import Pagination from 'react-bootstrap/Pagination';
-// Local imports
+import { Button, Card, Col, Row } from 'react-bootstrap';
 import './InstanceModels.css';
 import '../Pagination.css';
 
 const ARTWORKS_NUM_PAGES = 888;
 export const ARTWORKS_NUM_IDS = 7986;
 
+class Artworks extends Component {
+  state = {
+    databaseResponse: [],
+    data: [],
+    pageIndex: 1,
+  };
 
-export default class Artworks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      databaseResponse: [],
-      data: [],
-      pageIndex: 1,
-    };
-    this.handleClick = this.handleClick.bind(this);
+  componentDidMount() {
+    this.getResponseData(1);
   }
 
-  handleClick (clickAction) {
-    console.log("SELECTED: ", clickAction.selected)
-    let selected = clickAction.selected + 1;
-    this.setState({ pageIndex: selected })
-    this.getResponseData(selected)
-  }
+  handleClick = (clickAction) => {
+    const selected = clickAction.selected + 1;
+    this.setState({ pageIndex: selected });
+    this.getResponseData(selected);
+  };
 
   getResponseData = (targetIndex) => {
-    // TODO PHASE 3 do some checks to make sure within range
-    axios.get(`https://galleryguide.me/api/artworks?page=${ targetIndex }`)
-      .then(response => {
-        console.log(this.url)
-        const responseData = response.data
-        console.log("response data:")
-        console.log(responseData)
-        console.log("artwork data:")
-        console.log(responseData.artworks)
-
-        this.setState({ databaseResponse: responseData })
-        this.setState({ data: responseData.artworks })
+    axios
+      .get(`https://galleryguide.me/api/artworks?page=${targetIndex}`)
+      .then((response) => {
+        const responseData = response.data;
+        this.setState({ databaseResponse: responseData, data: responseData.artworks });
       })
       .catch((error) => {
-        console.log("axios error: ", error)
-      })
-  }
-
-  // Run once the page has loaded
-  componentDidMount() {
-    console.log("page loaded")
-    this.getResponseData(1)
-  }
+        console.log("axios error: ", error);
+      });
+  };
 
   render() {
+    const { pageIndex, data } = this.state;
+
     return (
       <div>
         <h1>Artworks</h1>
-        <p>Showing page { this.state.pageIndex }/{ ARTWORKS_NUM_PAGES }, 9/{ ARTWORKS_NUM_IDS } artworks.</p>
-        {<ReactPaginate
+        <p>Showing page {pageIndex}/{ARTWORKS_NUM_PAGES}, 9/{ARTWORKS_NUM_IDS} artworks.</p>
+        <ReactPaginate
           breakLabel={'...'}
           nextLabel={<Pagination.Next />}
           onPageChange={this.handleClick}
@@ -76,29 +60,28 @@ export default class Artworks extends React.Component {
           pageRangeDisplayed={5}
           previousClassName={'item previous'}
           pageCount={ARTWORKS_NUM_PAGES}
-          previousLabel={<Pagination.Prev /> }
-          renderOnZeroPageCount={null} 
-        />}
-        {
-          <Row xs={ 1 } md={ 3 } className="g-4">
-            { this.state.data.map(entry => (
-              <Col>
-                <Card style={{ justifyContent: 'center' }} key={ entry.id }>
-                  <Card.Img variant="top" src={ entry.image } />
-                  <Card.Body>
-                    {/* TODO #? add 5 sortable features to card */}
-                    <Card.Title>{ entry.title }</Card.Title>
-                    <Card.Text>{ entry.medium }</Card.Text>
-                    <Link to={`/artworks/${ entry.id }`}>
-                      <Button>Explore More</Button>
-                    </Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )) }
-          </Row>
-        }
+          previousLabel={<Pagination.Prev />}
+          renderOnZeroPageCount={null}
+        />
+        <Row xs={1} md={3} className="g-4">
+          {data.map((entry) => (
+            <Col key={entry.id}>
+              <Card style={{ justifyContent: 'center' }}>
+                <Card.Img variant="top" src={entry.image} />
+                <Card.Body>
+                  <Card.Title>{entry.title}</Card.Title>
+                  <Card.Text>{entry.medium}</Card.Text>
+                  <Link to={`/artworks/${entry.id}`}>
+                    <Button>Explore More</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
-    )
+    );
   }
 }
+
+export default Artworks;
