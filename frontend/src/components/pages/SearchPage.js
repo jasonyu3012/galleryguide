535f2328afce46bddd6851cd4214cb275cf80391
Tara Sophia Roshan <troshan@utexas.edu>
2023-03-29 22:04:35 -0500

    add sitewide search highlighting

--- frontend/src/components/pages/SearchPage.js
+++ frontend/src/components/pages/SearchPage.js
@@ -4,13 +4,22 @@ import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Col, Container, Row, Tab, Tabs, Card, Button } from "react-bootstrap";
 import axios from 'axios';
+import { Highlight } from "react-highlight-regex";
 
 const SearchPage = () => {
   let query = useParams().query;
+  let queryRE = new RegExp(`(?:${query.replaceAll(" ", "|")})`, "i");
   const [artworkData, setArtworkData] = useState({});
   const [artistData, setArtistData] = useState({});
   const [galleryData, setGalleryData] = useState({});
   console.log("query search: ", query)
+
+  function highlightText (input, regex) {
+    if (regex != null && input != null) {
+      return <Highlight match={regex} text={input} />
+    }
+    return input;
+  }
 
   useEffect(() => {
     // artworks
@@ -66,15 +75,16 @@ const SearchPage = () => {
       <Tabs defaultActiveKey="Artworks">
         <Tab eventKey="Artworks" title="Artworks">
           <Row xl={4} lg={3} md={2} sm={1} xs={1}>
-          {console.log("artwork data in row: ", artworkData)}
-          {console.log(artworkData.length === undefined)}
+          {console.log("queryRE: ", queryRE)}
+          {console.log(artworkData.length !== undefined)}
+          {console.log(artworkData)}
           { artworkData.length !== undefined ? artworkData.map(entry => (
             <Col>
               <Card style={{ justifyContent: 'center' }} key={ entry.id }>
                 <Card.Img variant="top" src={ entry.image } />
                 <Card.Body>
-                  <Card.Title>{ entry.title }</Card.Title>
-                  <Card.Text>{ entry.medium }</Card.Text>
+                  <Card.Title>{ highlightText(entry.title, queryRE) }</Card.Title>
+                  <Card.Text>{ highlightText(entry.medium, queryRE) }</Card.Text>
                   <Card.Text>ID #{ entry.id } | made in { entry.date } by artist ID {entry.artist_id}</Card.Text>
                   <Link to={`/artworks/${ entry.id }`}>
                     <Button>Explore More</Button>
@@ -92,8 +102,8 @@ const SearchPage = () => {
               <Card style={{ justifyContent: 'center' }} key={ entry.id }>
                 <Card.Img variant="top" src={ entry.thumbnail } />
                 <Card.Body>
-                  <Card.Title>{ entry.name }</Card.Title>
-                  <Card.Text>{ entry.medium }</Card.Text>
+                  <Card.Title>{ highlightText(entry.name, queryRE) }</Card.Title>
+                  <Card.Text>{ highlightText(entry.medium, queryRE) }</Card.Text>
                   <Card.Text>{ entry.birth_year } - { entry.death_year ? entry.death_year : "Present" }</Card.Text>
                   <Card.Text>ID #{ entry.id } | { entry.num_artworks } artworks | { entry.num_galleries } gallery</Card.Text>
                   <Link to={`/artists/${ entry.id }`}>
@@ -112,8 +122,8 @@ const SearchPage = () => {
               <Card style={{ justifyContent: 'center' }} key={ entry.id }>
                 <Card.Img variant="top" src={ entry.thumbnail } />
                 <Card.Body>
-                  <Card.Title>{ entry.name }</Card.Title>
-                  <Card.Text>{ entry.region }</Card.Text>
+                  <Card.Title>{ highlightText(entry.name, queryRE) }</Card.Title>
+                  <Card.Text>{ highlightText(entry.region, queryRE) }</Card.Text>
                   <Card.Text>ID #{ entry.id } | { entry.num_artworks } artworks | { entry.num_artists } artists</Card.Text>
                   <Link to={`/galleries/${ entry.id }`}>
                     <Button>Explore More</Button>
