2258251cb8717f27fcfdee21af32be74a0d76c32
Jerry Ming <jerryming2002@gmail.com>
2023-04-13 17:45:27 -0500

    more tests

--- frontend/src/__tests__/test.js
+++ frontend/src/__tests__/test.js
@@ -38,6 +38,51 @@ it("displays the artwork correctly", () => {
   expect(tree).toMatchSnapshot();
 });
 
+it("displays the galleries correctly", () => {
+  const gallerySample = {
+    id: 1,
+    name: "Name",
+    region: "region",
+    num_artists: 1, 
+    num_artworks: 1, 
+    thumbnail: "https://example.com/image.jpg",
+    website: "examplesite.com",
+    description: "description"
+  };
+
+  const component = renderer.create(
+    <BrowserRouter>
+      <GalleryInstance galleryData={gallerySample} />
+    </BrowserRouter>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("displays the artists correctly", () => {
+  const artistSample = {
+    id: 1,
+    name: "Name",
+    birth_year: 0,
+    death_year: 1, 
+    num_artworks: 1,
+    num_galleries: 1, 
+    thumbnail: "https://example.com/image.jpg",
+    biography: "biography"
+  };
+
+  const component = renderer.create(
+    <BrowserRouter>
+      <ArtistInstance artistData={artistSample} />
+    </BrowserRouter>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
 
 
 it("Navbar initializes correctly", () => {
